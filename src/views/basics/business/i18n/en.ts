1c54d4a5e68a01a46a86245de6607ebe65f39050
xueming <810505339@qq.com>
2023-11-08 17:31:49 +0800

    添加

---
+++ src/views/basics/business/i18n/en.ts
@@ -0,0 +1,8 @@
+export default {
+	agreement: {
+		namechina: 'agreement name(chinese)',
+		nameenglish: 'agreement name(english)',
+		contextenglish: 'agreement context(english)',
+		contextchina: 'agreement context(chinese)',
+	},
+};
