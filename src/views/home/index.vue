09a6918f6aaaba4e7bb528c37ad39f6ddbfff603
xueming <810505339@qq.com>
2023-11-27 20:57:53 +0800

    ADD

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex p-2.5 absolute inset-0 ">
     <div class="flex-auto border border-[#FFFFFF40] bg-[#00000040] rounded-xl mr-5 overflow-hidden p-6">
-      <div></div>
-      <div class="flex ">
-
+      <div class="h-6"></div>
+      <div class="flex h-[326px] gap-6">
+        <div class="flex-1 rounded-3xl bg-[#72AA3D80]"></div>
+        <div class="flex-1 rounded-3xl bg-[#CB7B2480]"></div>
+        <div class="flex-1 rounded-3xl bg-[#3F4FD780]"></div>
       </div>
 
     </div>
@@ -14,3 +16,6 @@
 <script setup lang="ts" name="home">
 
 </script>
+
+<style scoped lang="scss">
+</style>
