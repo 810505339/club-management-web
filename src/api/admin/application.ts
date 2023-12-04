6e2450a43d9f1bfae50cef4d025f30b2828034b7
xueming <810505339@qq.com>
2023-12-04 11:23:40 +0800

    add

---
+++ src/api/admin/application.ts
@@ -0,0 +1,38 @@
+
+import request from "/@/utils/request";
+
+
+export const getApplicationPage = async (params: any) => {
+  const { data } = await request({
+    url: '/admin/applicationVersion/page',
+    method: 'get',
+    params: params
+  });
+  return data
+}
+
+
+export const addApplication = async () => {
+  const { data } = await request({
+    url: '/admin/applicationVersion',
+    method: 'get'
+  });
+  return data
+}
+
+export const editApplication = async () => {
+  const { data } = await request({
+    url: '/admin/applicationVersion',
+    method: 'PUT'
+  });
+  return data
+}
+
+
+export const getApplicationById = async (id: string) => {
+  const { data } = await request({
+    url: `/admin/applicationVersion/${id}`,
+    method: 'PUT'
+  });
+  return data
+}
